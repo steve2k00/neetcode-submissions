2392c0ef3d5faad89c2da4cdfc64871aa96540ae
steve2k00 <12345sdtv@gmail.com>
2026-06-11 16:26:28 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 01.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "hi"
+print(type(variable))
+variable = ["hi"]
+print(type(variable))
