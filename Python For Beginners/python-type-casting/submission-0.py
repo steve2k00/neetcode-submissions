4652a2911f8a146be55634d2469979ccd3dd7224
steve2k00 <12345sdtv@gmail.com>
2026-06-11 16:31:43 -0400

    Add: python-type-casting - submission-0

---
+++ Python For Beginners/python-type-casting/submission-0.py
@@ -0,0 +1,9 @@
+pi = 3.141592653589793
+square_root_8 = 2.8284271247461903
+# Do not modify above this line
+pi = int(3)
+square_root_8 = int(2)
+
+# Do not modify below this line
+print(pi)
+print(square_root_8)
